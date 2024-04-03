08bd32919e8a9ed5a5e37be209eb2214125c5253
Ashkan Mosallanejad <ashkan.mosalla76@gmail.com>
2024-04-03 18:54:59 +0430

    Initial commit for number gusser project

---
+++ number_gusser/src/game_logic/scorer.py
@@ -0,0 +1,10 @@
+class Scorer:
+    def __init__(self, initial_score= 100):
+        self.score = initial_score
+
+    def Decrement_score(self, penalty= 10):
+        self.score = penalty
+        self.score = max(self.score, 0)
+    
+    def get_score(self):
+        return self.score
